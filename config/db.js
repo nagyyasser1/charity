1faca4ee3bf0a49967c085eb1c10746db2dbf714
nagy <nagyy8751@gmail.com>
2023-08-29 09:28:40 +0200

    v2

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.NEW_DB, {
+    await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
